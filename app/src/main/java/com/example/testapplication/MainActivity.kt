1be6a6c38b97ebbf6e78822cb0aba5f30c8ba5bc
jacopo.cafarelli <jacopo.cafarelli@xing.com>
2023-09-07 14:34:37 +0200

    Initial commit

---
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -0,0 +1,153 @@
+package com.example.testapplication
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidViewBinding
+import com.example.testapplication.databinding.FragmentMainContainerBinding
+import com.example.testapplication.ui.theme.TestApplicationTheme
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TestApplicationTheme {
+                MainScreen()
+            }
+        }
+    }
+
+    @Composable
+    private fun MainScreen(
+        drawerState: DrawerState = rememberDrawerState(initialValue = DrawerValue.Closed),
+    ) {
+        ModalNavigationDrawer(
+            drawerContent = { DrawerContent() },
+            drawerState = drawerState,
+        ) {
+            Scaffold(
+                topBar = { TopBar(drawerState) }
+            ) { paddingValues ->
+                val modifier = Modifier.padding(
+                    start = paddingValues.calculateStartPadding(LayoutDirection.Ltr) + 16.dp,
+                    top = paddingValues.calculateTopPadding() + 8.dp,
+                    end = paddingValues.calculateEndPadding(LayoutDirection.Ltr) + 16.dp,
+                    bottom = paddingValues.calculateBottomPadding() + 8.dp
+                )
+                FragmentContentWithColumn(modifier)
+                //FragmentContentWithoutColumn(modifier)
+                //GreetingWithColumn(modifier)
+                //GreetingWithoutColumn(modifier)
+            }
+        }
+    }
+
+    @Composable
+    private fun FragmentContentWithColumn(modifier: Modifier) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
+        ) {
+            AndroidViewBinding(
+                factory = FragmentMainContainerBinding::inflate,
+                modifier = modifier.fillMaxSize()
+            )
+        }
+    }
+
+    @Composable
+    private fun FragmentContentWithoutColumn(modifier: Modifier) {
+        AndroidViewBinding(
+            factory = FragmentMainContainerBinding::inflate,
+            modifier = modifier.fillMaxSize()
+        )
+    }
+
+    @Composable
+    fun GreetingWithColumn(modifier: Modifier = Modifier) {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
+        ) {
+            Text(
+                text = "Hello!",
+                modifier = Modifier
+            )
+        }
+    }
+
+    @Composable
+    fun GreetingWithoutColumn(modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello!",
+            modifier = modifier
+        )
+    }
+
+    @Composable
+    @OptIn(ExperimentalMaterial3Api::class)
+    private fun TopBar(drawerState: DrawerState) {
+        val coroutineScope = rememberCoroutineScope()
+        TopAppBar(
+            title = {}, // no title
+            navigationIcon = {
+                IconButton(
+                    onClick = { coroutineScope.launch { drawerState.open() } }
+                ) {
+                    Icon(
+                        Icons.Rounded.Menu,
+                        contentDescription = "MenuButton"
+                    )
+                }
+            },
+        )
+    }
+}
+
+@Composable
+fun DrawerContent(modifier: Modifier = Modifier) {
+    ModalDrawerSheet(modifier.width(260.dp)) {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+            ) {
+                Text(text = "First element", modifier = Modifier.padding(8.dp))
+                Text(text = "Second element", modifier = Modifier.padding(8.dp))
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/testapplication/MainFragment.kt
@@ -0,0 +1,24 @@
+package com.example.testapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.testapplication.databinding.FragmentMainContentBinding
+
+class MainFragment: Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return FragmentMainContentBinding.inflate(inflater, container, false).root
+    }
+
+}
